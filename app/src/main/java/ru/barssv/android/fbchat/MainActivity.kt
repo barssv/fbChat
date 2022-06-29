fcc46b344f7d080c0c83d921f58c03b9b5e57fc7
barssv <barssv@mail.ru>
2022-06-29 20:54:15 +1000

    создан дата класс, осуществляется передача данных в Firebase из активити(4 параметра стринг) с созданием ветки по логину

--- app/src/main/java/ru/barssv/android/fbchat/MainActivity.kt
+++ app/src/main/java/ru/barssv/android/fbchat/MainActivity.kt
@@ -2,16 +2,50 @@ package ru.barssv.android.fbchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import ru.barssv.android.fbchat.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var datebase: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val database = Firebase.database
-        val myRef = database.getReference("message")
-        myRef.setValue("Hello, ")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+
+            val firstname = binding.name.text.toString()
+            val lastname = binding.lastname.text.toString()
+            val age = binding.age.text.toString()
+            val login = binding.login.text.toString()
+
+            datebase = FirebaseDatabase.getInstance().getReference("Users")
+            val User = User(firstname, lastname, age, login)
+            datebase.child(login).setValue(User).addOnSuccessListener {
+                binding.name.text.clear()
+                binding.lastname.text.clear()
+                binding.age.text.clear()
+                binding.login.text.clear()
+
+                Toast.makeText(this,"Успешно сохранено", Toast.LENGTH_LONG).show()
+            }.addOnFailureListener {
+                    Toast.makeText(this,"Ошибка сохранения", Toast.LENGTH_LONG).show()
+                }
+
+        }
+
+//        val database = Firebase.database
+//        val myRef = database.getReference("message")
+//        myRef.setValue("Hello, ")
     }
 }
---
+++ app/src/main/java/ru/barssv/android/fbchat/User.kt
@@ -0,0 +1,8 @@
+package ru.barssv.android.fbchat
+
+data class User(
+    val firstname: String? = null,
+    val lastname: String? = null,
+    val age: String? = null,
+    val login: String? = null
+)
